d5bc35db186d0396ad1ab844c47c5f32fdab7c37
articulatestudio <lewis@articulate-studio.com>
2017-05-06 17:37:50 +0100

    Trying to change to new database

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 var settings = {
     "replyAiWebHook": "/handlers/external/received/d4a8dd5f-4eda-40b7-8dd5-8b2572d46590/",
     "httpServerPort": process.env.PORT || 80,
-    "mongoUri": process.env.MONGODB_URI || "mongodb://lewis:lewis@ds127451-a0.mlab.com:27451/heroku_cllxmt07",
+    "mongoUri": process.env.MONGODB_URI || "mongodb://ds133771-a0.mlab.com:33771/heroku_b2v9lgzf",
     "xmlUrls" : [
         "http://www.goalserve.com/getfeed/e2b9882859c64657bae5615d1a29026b/racing/uk",
         "http://www.goalserve.com/getfeed/e2b9882859c64657bae5615d1a29026b/racing/uk_tomorrow"
