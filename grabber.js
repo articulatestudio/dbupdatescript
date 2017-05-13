542950469039f1e1befab9e16648d39d11e82f9d
RuslanGromov <ruslan_gromov@hotmail.com>
2017-05-13 03:14:27 +0300

    Update grabber.js

--- grabber.js
+++ grabber.js
@@ -1,5 +1,6 @@
 const CronJob = require('cron').CronJob;
 const g2 = require('./g2');
+const g3 = require('./g3');
 // g2.run();
 
 console.log('Starting worker - ', Date());
@@ -10,6 +11,17 @@ new CronJob(
   function() {
     console.log('Starting task', Date());
     g2.run();
+  },
+  null,
+  true
+);
+
+// Init cron to run grabber every minute
+new CronJob(
+  cronTime: '00 3- 03 * * * *',
+  onTick: function() {
+    console.log('Starting task', Date());
+    g3.run();
   },
   null,
   true
